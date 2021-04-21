0855eaf1ac8c579dcb48402190cb2c08c063696f
Gon Solo <gonsolo@gmail.com>
2021-04-21 19:48:37 +0200

    Clean up.

--- Sources/MdlPEGParser/main.swift
+++ Sources/MdlPEGParser/main.swift
@@ -1,45 +1,10 @@
 import Foundation
 import SwiftPEG
 
-let markdownSyntax = #"""
-    raw_text = ~"[^\n]+"
-    bold_text = ("**" raw_text "**") / ("__" raw_text "__")
-    text = (bold_text / raw_text)
-
-    h1 = "# " text
-    h2 = "## " text
-    h3 = "### " text
-    h4 = "#### " text
-    h5 = "##### " text
-    h6 = "######" text
-    header = (h6 / h5 / h4 / h3 / h2 / h1)
-
-    ordered_list = (~"[0~9]+\. " text ~"\n")+
-
-    unordered_list = (~"[-*+] " text ~"\n")+
-
-    link = "[" raw_text "]" "(" raw_text ")"
-
-    image = "![" raw_text "]" "(" raw_text ")"
-
-    paragraph = (header / text)?
-    doc = (paragraph ~"\n\n")* paragraph
-"""#
-let markdownText = "# bla bla ## bla ### bla"
-let markdownParser: Grammar = Grammar(rules: markdownSyntax)
-
 // [] optional
 // {} zero or more
 
-let testSyntax = #"""
-        root = ab+
-        ab = a / b
-        a = "a"
-        b = "b"
-"""#
-
-
-let mdlSyntax = #"""
+let syntax = #"""
         root = argument_list
 
         argument_list = "(" _
@@ -54,16 +19,13 @@ let mdlSyntax = #"""
 
         positional_named_arguments = positional_argument additional_positional_argument* additional_named_argument*
         positional_argument = "pos" _
-        additional_positional_argument = "," _ positional_argument
+        additional_positional_argument = "," _ positional_argument _
+
         _ = whitespace
         whitespace = ~"\s*"
 """#
 
-//let text = "(bla: laber, bla: laber)"
-let text = "(pos, pos, bla: laber)"
-
-//let syntax = testSyntax
-let syntax = mdlSyntax
+let text = "(pos, pos, bla: laber, bla: laber)"
 
 let parser = Grammar(rules: syntax)
 
